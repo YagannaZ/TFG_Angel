609cca2da45d26a6d1f4ee417d2e5a17f207258a
YagannaZ <anhell2405@gmail.com>
2023-06-09 13:48:21 +0200

    Eso

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<div class="P1-wrapper">
-    <div class="primer-div">
-        <p>Primer DIV creado en el proyecto de clase</p>    
-    </div>
-    
-</div>
+
 
 <style>
-    .P1-wrapper
-    {
-        
-        width: 5cqw;
-        height: auto;
-        font-size: 2cqw;
-    }
+
 </style>
