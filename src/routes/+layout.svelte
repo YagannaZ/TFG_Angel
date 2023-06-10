33dc3452765626c803c552e24f61876f112dc216
YagannaZ <anhell2405@gmail.com>
2023-06-10 13:25:45 +0200

    Añadido style global Header FOOTER y estructura de layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import "$lib/styles/style.scss"
+    import Footer from "$lib/componentes/FOOTER.svelte";
+    import Header from "$lib/componentes/HEADER.svelte";
+</script>
+
+<Header></Header>
+
+<slot/>
+
+<Footer></Footer>
+<style>
+
+</style>
